1749d7b4ac41b6f3be193e7f333d626e4cf14205
ashok_2216 <ashoksiva2216.com>
2022-11-22 12:28:12 +0530

    file added

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 # Import packages
+import os
 import cv2
 import numpy as np
- 
+
+datadir = 'images'
+# print(len(os.listdir(datadir)))
+for i in range(len(os.listdir(datadir))):
+    print(i)
+    path = os.path.join(datadir,)
+    for img in os.listdir(datadir):
+        img_arr = cv2.imread(os.path.join(datadir,img))
+
+
+
+
 img = cv2.imread('images/result_Page_1.jpg')
 print(img.shape) # Print image shape
 cv2.imshow("original", img)
@@ -13,7 +25,9 @@ cropped_image = img[100:210, 410:730]
 cv2.imshow("cropped", cropped_image)
  
 # Save the cropped image
-cv2.imwrite("croped_img/Cropped Image.jpg", cropped_image)
+for i in range(10):
+    print(i)
+    cv2.imwrite("croped_img/Device_name/Cropped_Image{}.jpg" .format(i), cropped_image)
  
 cv2.waitKey(0)
 cv2.destroyAllWindows()
