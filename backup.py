ad0bf8d5c5f12360bc081e4feca96b83242f3a9b
ashok_2216 <ashoksiva2216.com>
2022-11-23 13:37:09 +0530

    code added

--- backup.py
+++ backup.py
@@ -12,7 +12,19 @@ for i in range(len(os.listdir(datadir))):
     path = os.path.join(datadir,filename)
     print(path)
     img_path = path
-    img_array = cv2.imread(img_path)
+    # img_array = cv2.imread(img_path)
+    img_array = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+    # print('Original Dimensions : ',img_array.shape)
+    
+    scale_percent = 80 # percent of original size
+    width = int(img_array.shape[1] * scale_percent / 100)
+    height = int(img_array.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    
+    # resize image
+    img_array = cv2.resize(img_array, dim, interpolation = cv2.INTER_AREA)
+    
+    # print('Resized Dimensions : ',img_array.shape)
     # ref_axis, ref_box = 
     # device_axis, device_box = ((370,180),(270+380,110+8))
     # lot_axis, lot_box = ((220,320),(100+380,210+160))
@@ -32,19 +44,19 @@ for i in range(len(os.listdir(datadir))):
             img=cv2.rectangle(img_array,(x,y),(x+w,y+h),(0,205,100),2)
         else:
             print('Correct Type!')
-        boxed_img = cv2.imwrite("boxed_img.jpg" .format(i), img)
-        x=x+1
-        y=y+1
+        boxed_img = cv2.imwrite("boxed_img{}.jpg" .format(i), img)
+        x=x+8
+        y=y+8
         crop_img = img_array[y:y+h, x:x+w]
         return crop_img, img, boxed_img
 
-    cropped_ref_img = np.array(crop(210,200,380,110, img_type='ref'))
+    cropped_ref_img = crop(210,280,380,110, img_type='ref')
     cropped_device_img = crop(370,180,380,8, img_type='device')
     cropped_lot_img = crop(220,320,380,160, img_type='lot')
     cropped_qty_img = crop(1080,220,980,110, img_type='qty')
     cropped_symbol_img = crop(1060,220,920,110, img_type='symbol')
-    print(cropped_ref_img)
-    cv2.imwrite("cropped_ref_img.jpg", cropped_ref_img)
+    # print(cropped_ref_img[0])
+    cv2.imwrite("cropped_ref_img.jpg", cropped_ref_img[0])
     cv2.imwrite("Image_output\device\cropped_device_img.jpg",cropped_device_img)
     cv2.imwrite("Image_output\lot\cropped_lot_img.jpg",cropped_lot_img)
     cv2.imwrite("Image_output\qty\cropped_qty_img.jpg", cropped_qty_img)
